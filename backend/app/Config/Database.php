8e388a480781557654a6a6e6a9a5743a7597981a
chalsimos <krizchan31@gmail.com>
2023-12-14 20:44:16 +0800

    update pass

--- backend/app/Config/Database.php
+++ backend/app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'root',
-        'password'     => '',
-        'database'     => 'med',
+        'username'     => 'u166719925_posh',
+        'password'     => 'Posh@Sh!t1',
+        'database'     => 'u166719925_pos',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
